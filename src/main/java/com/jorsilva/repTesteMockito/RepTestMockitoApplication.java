1544a6ba9d4c613a8dad8b5633c9550922206989
Jordan <jordanrafael92@gmail.com>
2021-11-04 19:51:21 -0300

    Created Project

---
+++ src/main/java/com/jorsilva/repTesteMockito/RepTestMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.jorsilva.repTesteMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepTestMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepTestMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorsilva/repTesteMockito/RepTestMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorsilva.repTesteMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepTestMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
