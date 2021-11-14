04a9933469387da608d5d15402fe357b43768f03
Jordan <jordanrafael92@gmail.com>
2021-11-14 10:26:34 -0300

    Create test integration City - Order by name

--- src/main/java/com/jorsilva/repTesteMockito/entities/Event.java
+++ src/main/java/com/jorsilva/repTesteMockito/entities/Event.java
@@ -2,12 +2,16 @@ package com.jorsilva.repTesteMockito.entities;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_event")
 public class Event {
 
 	@Id
