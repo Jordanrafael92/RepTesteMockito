bc8b9a3f0f59b2fbf8fb5e4ff74d7f89cda2a0ad
Jordan <jordanrafael92@gmail.com>
2021-11-14 12:11:56 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO tb_city(id, name) VALUES (1, 'São Paulo');
-INSERT INTO tb_city(id, name) VALUES (2, 'Brasília');
-INSERT INTO tb_city(id, name) VALUES (3, 'Fortaleza');
-INSERT INTO tb_city(id, name) VALUES (1, 'Salvador');
-INSERT INTO tb_city(id, name) VALUES (2, 'Manaus');
-INSERT INTO tb_city(id, name) VALUES (3, 'Curitiba');
-INSERT INTO tb_city(id, name) VALUES (1, 'Goiânia');
-INSERT INTO tb_city(id, name) VALUES (2, 'Belém');
-INSERT INTO tb_city(id, name) VALUES (3, 'Porto Alegre');
-INSERT INTO tb_city(id, name) VALUES (1, 'Rio de Janeiro');
-INSERT INTO tb_city(id, name) VALUES (2, 'Belo Horizonte');
+INSERT INTO tb_city(name) VALUES ('São Paulo');
+INSERT INTO tb_city(name) VALUES ('Brasília');
+INSERT INTO tb_city(name) VALUES ('Fortaleza');
+INSERT INTO tb_city(name) VALUES ('Salvador');
+INSERT INTO tb_city(name) VALUES ('Manaus');
+INSERT INTO tb_city(name) VALUES ('Curitiba');
+INSERT INTO tb_city(name) VALUES ('Goiânia');
+INSERT INTO tb_city(name) VALUES ('Belém');
+INSERT INTO tb_city(name) VALUES ('Porto Alegre');
+INSERT INTO tb_city(name) VALUES ('Rio de Janeiro');
+INSERT INTO tb_city(name) VALUES ('Belo Horizonte');
 
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Feira do Software', '2021-05-16', 'https://feiradosoftware.com', 1);
-INSERT INTO tb_event(name, date, url, city_id) VALUES ('CCXP', '2021-04-13', 'https://ccxp.com.br', 1);
+INSERT INTO tb_event(name, date, url, city_id) VALUES ('CCXP', '2021-04-13', 'https://ccxp.com.br', 4);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Congresso Linux', '2021-05-23', 'https://congressolinux.com.br', 2);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Semana Spring React', '2021-05-03', 'https://devsuperior.com.br', 3);
